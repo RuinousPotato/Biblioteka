733ede83e1d2aa725b48c98126e660c01f44924e
Ruinous Potato <fusiek@gmail.com>
2018-04-22 20:45:48 +0200

    added pictures to search page

---
+++ surprise.js
@@ -0,0 +1,27 @@
+/*
+function showElefant() {
+    var x = document.getElementById("elefant");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+    } else {
+        x.style.visibility = "hidden";
+    }
+}
+
+function showElephant() {
+    var x = document.getElementById("elephant");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+    } else {
+        x.style.visibility = "hidden";
+    }
+}
+*/
+function showElem(elem) {
+    var x = document.getElementById(elem);
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }
+}
