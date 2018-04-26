7f9c709ec98a8958df4ab3efa9e999ed052ea3b9
Ruinous Potato <fusiek@gmail.com>
2018-04-26 23:32:05 +0200

    updateBooks code change 01

--- UpdateBooks.php
+++ UpdateBooks.php
@@ -1,101 +1,36 @@
 <?php
-
-class UpdateBooks {
-
-    const DB_HOST = 'localhost';
-    const DB_NAME = 'biblioteka';
-    const DB_USER = 'root';
-    const DB_PASSWORD = 'root123';
-
-    /**
-     * PDO instance
-     * @var PDO
-     */
-    private $pdo = null;
-
-    /**
-     * Open the database connection
-     */
-    public function __construct() {
-        // open database connection
-        $connStr = sprintf("mysql:host=%s;dbname=%s", self::DB_HOST, self::DB_NAME);
-        try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=biblioteka', self::DB_USER, self::DB_PASSWORD);
-        } catch (PDOException $e) {
-            echo "not workind demet";
-            die($e->getMessage());
-        }
-        var_dump($pdo);
-    }
-
-
-    /*
-    try {
-    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
-    foreach($dbh->query('SELECT * from FOO') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
-    } catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-    }
-    */
-
-    /**
-     * Update an existing task in the tasks table
-     * @param string $subject
-     * @param string $description
-     * @param string $startDate
-     * @param string $endDate
-     * @return bool return true on success or false on failure
-     */
-
-    public function update($ID, $tytul, $autor, $data, $wydawnictwo, $isbn, $gatunek, $lokalizacja) {
-        $ksiazka = [
-            ':ID' => $ID,
-            ':tytul' => $tytul,
-            ':autor' => $autor,
-            ':data' => $data,
-            ':wydawnictwo' => $wydawnictwo,
-            ':isbn' => $isbn,
-            ':gatunek' => $gatunek,
-            ':lokalizacja' => $lokalizacja];
-
-
-        $sql = 'UPDATE ksiazki_arkonska
-                    SET tytul = :tytul,
-                        autor = :autor,
-                        data = :data,
-                        wydawnictwo = :wydawnictwo,
-                        isbn = :isbn,
-                        gatunek = :gatunek,
-                        lokalizacja = :lokalizacja
-                  WHERE ID = :ID';
-
-        $query = $this->pdo->prepare($sql);
-
-        return $query->execute($ksiazka);
-    }
-
-    /**
-     * close the database connection
-     */
-    public function __destruct() {
-        // close the database connection
-        $this->pdo = null;
-    }
-
+//print_r(PDO::getAvailableDrivers());
+try {
+  $pdo = new PDO('mysql:host=localhost;dbname=biblioteka','root','root123');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//print_r($handler);
+} catch(PDOException $e) {
+  echo $e->getMessage();
+  die();
 }
-$obj = new UpdateBooks();
 
-if ($obj->update(13, 'test04',
-                    'test04',
-                    'test04',
-                    'test04',
-                    'test04',
-                    'test04',
-                    'test04') !== false)
-    echo 'The task has been updated successfully';
-else
-    echo 'Error updated the task';
+class updateBook {
+    public $tytul, $autor, $wydawnictwo;
+}
+
+$query = $pdo->query('SELECT * FROM ksiazki_arkonska');
+$query->setFetchMode(PDO::FETCH_CLASS, 'updateBook');
+ //$row = $query->fetch(PDO::FETCH_ASSOC);
+ //echo '<pre>',print_r($row), '</pre>';
+
+while($row = $query->fetch(PDO::FETCH_OBJ)){
+  //echo $row->Tytul, '<br>';
+  echo '<pre>', print_r($row), '</row>';
+}
+
+
+/*
+$db_update = $pdo->prepare("INSERT INTO form_input (name, email, phone)
+VALUES (:name, :email, :phone)");
+$db_update->bindParam(':name', $name);
+$db_update->bindParam(':email', $email);
+$db_update->bindParam(':phone', $phone);
+$db_update->execute();
+*/
+
+?>
