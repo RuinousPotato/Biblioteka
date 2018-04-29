1664aecfe7bf08d780486427ed548cc2af5bdf95
Ruinous Potato <fusiek@gmail.com>
2018-04-29 22:40:25 +0200

    UpdateBooks code change 03

--- UpdateBooks.php
+++ UpdateBooks.php
@@ -1,5 +1,55 @@
 <?php
 
+if (isset($_POST['submit'])) {
+
+    print_r($_REQUEST);
+    echo "<br>";
+try {
+    $pdo=new PDO("mysql:host=localhost;dbname=biblioteka", "root", "root123");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $tytul = $_POST['Tytul'];
+    $autor = $_POST['Autor'];
+    $data = $_POST['Data_Wydania'];
+    $wydawnictwo = $_POST['Wydawnictwo'];
+    $ISBN = $_POST['ISBN'];
+    $gatunek = $_POST['Gatunek'];
+    $lokalizacja = $_POST['Lokalizacja'];
+
+    //$query = "UPDATE ksiazki_arkonska SET Tytul='"$name . "',Autor='" . $email . "',Data='" . $pos . "',Wydawnictwo='" . $wydawnictwo . "',ISBN='" . $ISBN . "',Gatunek='" . $gatunek . "',Lokalizacja='" . $lokalizacja . "' WHERE ID=".$id;
+    //$query = "UPDATE ksiazki_arkonska SET Tytul='$tytul',Autor='$autor',Data='$data',Wydawnictwo='$wydawnictwo',ISBN='$ISBN',Gatunek='$gatunek',Lokalizacja='$lokalizacja' WHERE ID='$id'";
+
+    $query = $pdo->prepare("UPDATE `ksiazki_arkonska` SET `Tytul` = ':tytul', `Autor` = ':autor', `Data_wydania` = ':data', `Wydawnictwo` = ':wydawnictwo', `ISBN` = ':ISBN', `Gatunek` = ':gatunek', `Lokalizacja` = ':lokalizacja' WHERE `ID` = ':id'");
+    print_r($query);
+    echo "<br>";
+    $query->bindParam(':id', $id);
+    $query->bindParam(':tytul', $tytul);
+    $query->bindParam(':autor', $autor);
+    $query->bindParam(':data', $data);
+    $query->bindParam(':wydawnictwo', $wydawnictwo);
+    $query->bindParam(':ISBN', $ISBN);
+    $query->bindParam(':gatunek', $gatunek);
+    $query->bindParam(':lokalizacja', $lokalizacja);
+    print_r($query);
+    echo "<br>";
+    $query->execute();
+
+    echo "<br>";
+    echo 'Book updated successfully';
+    // $update = updateBooks($query);
+    } catch (\Throwable $e) {
+      echo $e->getMessage();
+    }
+/*    if ($query->execute() != 0) {
+
+    }else{
+        echo 'error in update query';
+    }
+*/
+}
+
+/*
 if(isset($_POST['update']))
 {
     try {
@@ -8,9 +58,9 @@ if(isset($_POST['update']))
         echo $e->getMessage();
         die();
     }
-    
+
     // get values form input text and number
-    
+
     $id = $_POST['id'];
     $tytul = $_POST['tytul'];
     $autor = $_POST['autor'];
@@ -19,15 +69,15 @@ if(isset($_POST['update']))
     $ISBN = $_POST['ISBN'];
     $gatunek = $_POST['gatunek'];
     $lokalizacja = $_POST['lokalizacja'];
-    
+
     // mysql query to Update data
-    
+
     $query = "UPDATE `ksiazki_arkonska` SET `Tytul`=:tytul,`Autor`=:autor,`Data`=:data,`Wydawnictwo`=:wydawnictwo,`ISBN`=:ISBN,`Gatunek`=:gatunek,`Lokalizacja`=:lokalizacja WHERE `ID` = :id";
-    
+
     $result = $pdo->prepare($query);
-    
+
     $pdoExec = $result->execute(array(":tytul"=>$tytul,":autor"=>$autor,":data"=>$data,":wydawnictwo"=>$wydawnictwo,":ISBN"=>$ISBN,":gatunek"=>$gatunek,":lokalizacja"=>$lokalizacja,":id"=>$id));
-    
+
     if($pdoExec)
     {
         echo 'Data Updated';
@@ -36,7 +86,7 @@ if(isset($_POST['update']))
     }
 
 }
-
+*/
 /*
 //print_r(PDO::getAvailableDrivers());
 try {
--- edit.php
+++ edit.php
@@ -1,47 +1,38 @@
 <?php
 
-include './include/config.php';
+//include './include/config.php';
 print_r($_REQUEST);
-if (isset($_POST['id'])) {
-    $id = $_POST['id'];
+if (isset($_REQUEST['id'])) {
+    $id = $_REQUEST['id'];
     echo $id;
 }
 
-$selectBook="SELECT * FROM ksiazki_arkonska WHERE id=$id";
+try {
+$pdo=new PDO("mysql:host=localhost;dbname=biblioteka", "root", "root123");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$book=selectBooks($selectBook);
+$selectBook="SELECT * FROM ksiazki_arkonska WHERE id = :id";
 
+$statement = $pdo->prepare($selectBook);
+$statement->bindValue(':id', $id);
+$statement->execute();
+$book = $statement->fetchAll();
 
-if (isset($_POST['submit'])) {
-
-    $tytul = $_POST['tytul'];
-    $autor = $_POST['autor'];
-    $data = $_POST['data'];
-    $wydawnictwo = $_POST['wydawnictwo'];
-    $ISBN = $_POST['ISBN'];
-    $gatunek = $_POST['gatunek'];
-    $lokalizacja = $_POST['lokalizacja'];
-
-    //$query = "UPDATE ksiazki_arkonska SET Tytul='"$name . "',Autor='" . $email . "',Data='" . $pos . "',Wydawnictwo='" . $wydawnictwo . "',ISBN='" . $ISBN . "',Gatunek='" . $gatunek . "',Lokalizacja='" . $lokalizacja . "' WHERE ID=".$id;
-    $query = "UPDATE ksiazki_arkonska SET Tytul='$name',Autor='$email',Data='$pos',Wydawnictwo='$wydawnictwo',ISBN='$ISBN',Gatunek='$gatunek',Lokalizacja='$lokalizacja' WHERE ID='$id'";
-
-    $update = updateBooks($query);
-
-    if ($update != 0) {
-        echo 'employe updated successfully';
-    }else{
-        echo 'error in update query';
-    }
+//$book=selectBooks($selectBook);
+} catch (\Throwable $e) {
+echo $e->getMessage();
 }
+
 ?>
 
-<form method="post">
-    <input type="text" name="Tytuł" value="<?php echo $book[0]['Tytul']; ?>"> <br>
+<form action="UpdateBooks.php" method="post">
+    <input type="hidden" name="id" value = "<?php echo $book[0]['ID']; ?>">
+    <input type="text" name="Tytul" value="<?php echo $book[0]['Tytul']; ?>"> <br>
     <input type="text" name="Autor" value="<?php echo $book[0]['Autor']; ?>"> <br>
-    <input type="text" name="Data Wydania" value="<?php echo $book[0]['Data_wydania']; ?>"> <br>
+    <input type="text" name="Data_Wydania" value="<?php echo $book[0]['Data_wydania']; ?>"> <br>
     <input type="text" name="Wydawnictwo" value="<?php echo $book[0]['Wydawnictwo']; ?>"> <br>
     <input type="text" name="ISBN" value="<?php echo $book[0]['ISBN']; ?>"> <br>
     <input type="text" name="Gatunek" value="<?php echo $book[0]['Gatunek']; ?>"> <br>
     <input type="text" name="Lokalizacja" value="<?php echo $book[0]['Lokalizacja']; ?>"> <br>
-    <input type="submit" name="submit" value="Edit">
+    <input type="submit" name="submit" value="Zapisz">
 </form>
