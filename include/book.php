3d6094a75bb31307153ae26c1fe31b2b62b75a5c
Ruinous Potato <fusiek@gmail.com>
2018-05-18 21:05:53 +0200

    added book class

---
+++ include/book.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ */
+class Book
+{
+
+  var $id;
+  var $tytul;
+  var $autor;
+  var $data;
+  var $wydawnictwo;
+  var $ISBN;
+  var $gatunek;
+  var $lokalizacja;
+
+
+
+  function __construct(argument)
+  {
+    // code...
+  }
+}
+
+
+ ?>
